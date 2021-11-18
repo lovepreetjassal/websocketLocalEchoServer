a71121c470a26923912071acf7bc64540afbebe8
Lovepreet <lovepreet.jassal@convergent.com>
2021-11-18 14:33:40 -0500

    Added more logging

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const wss = new WebSocket.Server({ server });
 server.listen(8080);
 
 wss.on('connection', function connection(ws) {
+	console.log("new connection");
     ws.on('message', function incoming(message) {
         let parsedJSON = JSON.parse(message);
         ws.send(JSON.stringify(parsedJSON));
